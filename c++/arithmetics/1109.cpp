acf144a88998b9dd403a9cab4a0413b9c1a33dc3
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-23 13:33:52 +0300

    Create 1109.cpp

---
+++ c++/arithmetics/1109.cpp
@@ -0,0 +1,11 @@
+//https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=26&id_problem=152
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    cout << "The next number for the number " << n << " is " << n+1 << ".\n";
+    cout << "The previous number for the number " << n << " is " << n-1 << ".";
+}
