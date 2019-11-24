0654e37ebcb588171bfad2c8ca3d17207d48c27f
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-24 20:37:19 +0300

    Create 0033.cpp

---
+++ 0033.cpp
@@ -0,0 +1,11 @@
+//https://acmp.ru/index.asp?main=task&id_task=33
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << b-1 << " " << a-1;
+    return 0;
+}
