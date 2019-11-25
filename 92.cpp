9b9c9737f797e72ce1d893110b4cfbbb329809aa
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2019-11-25 13:34:58 +0300

    Create 92.cpp

---
+++ 92.cpp
@@ -0,0 +1,11 @@
+//https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=1&id_topic=28&id_problem=150
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int s;
+    cin >> s;
+    cout << s/6.0 << " " << 2*s/3.0 << " " <<  s/6.0;
+    return 0;
+}
