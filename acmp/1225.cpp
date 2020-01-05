f5895fd683a08f70e7790aade160cc91adb705ec
Denis Mironov <45696712+dmironov1993@users.noreply.github.com>
2020-01-05 11:33:05 -0600

    Update 1225.cpp

--- acmp/1225.cpp
+++ acmp/1225.cpp
@@ -1,3 +1,4 @@
+//First solution by using predefined isdigit function
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,6 +8,33 @@ int main() {
         char a;
         cin >> a;
         if (isdigit(a)) {
+            cnt += 1;
+        }
+    }
+    cout << cnt;
+    return 0;
+}
+
+//Second solution by using created IsDigit function
+#include <bits/stdc++.h>
+using namespace std;
+
+bool IsDigit(char C) {
+    vector<char> vect{'0','1','2','3','4','5','6','7','8','9'};
+    for (char x: vect) {
+        if (x == C) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int cnt = 0;
+    for (int i = 0; i < 3; i++) {
+        char a;
+        cin >> a;
+        if (IsDigit(a)) {
             cnt += 1;
         }
     }
